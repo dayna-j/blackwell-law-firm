f4706e53be72671582a733ef53b3a485ba238d03
Dayna Blackwell <daynajblackwell@gmail.com>
2018-07-07 15:24:10 -0700

    Adjusted scroll reveal settings.
    Changed favicon.
    
    Added material icons down arrow to splash page.  I will be animating this icon to float up and down within it's container.

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -2,12 +2,24 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    var sr = ScrollReveal({
-        reset: true
+    var sr = ScrollReveal();
+
+    sr.reveal('#splash-dialog', {
+        duration: 1500,
+        reset: false
+    });
+
+    sr.reveal('.button-wrapper', {
+        duration: 1500,
+        reset: false
     });
 
     sr.reveal('.features-grid', {
         duration: 650,
-        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+        reset: true
     });
 });
+
+// #splash-dialog
+// .button-wrapper
--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    let sr = ScrollReveal({
-        reset: true
-    });
+    let sr = ScrollReveal();
     
 
     sr.reveal('#splash-dialog', {
-        duration: 1500
+        duration: 1500,
+        reset: false
+    });
+
+    sr.reveal( '.button-wrapper' , {
+        duration: 1500,
+        reset: false
     });
 
 
     sr.reveal('.features-grid', {
         duration: 650,
-        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+        reset: true
     });
 
 
