2e98ec6652fe4fb3ae871dac11636137e43f0122
Dayna Blackwell <daynajblackwell@gmail.com>
2018-07-07 01:01:30 -0700

    Added features section.  Populated features grid with images. Added scrollreveal.js to lazy load feature grif images.

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.sr = ScrollReveal({
+        reset: true
+    });
+
+    sr.reveal('.features-grid', {
+        duration: 500,
+        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+    });
+});
---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    window.sr = ScrollReveal({
+        reset: true
+    });
+    
+    sr.reveal('.features-grid', {
+        duration: 500,
+        easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)'
+    });
+
+
+  });
